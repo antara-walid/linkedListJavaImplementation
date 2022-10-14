02cd9ebe015a21cc24a775cc84901d3eb1fff7d6
antara-walid <Antara.walid.27@gmail.com>
2022-10-14 15:36:01 +0100

    linkedList first version

---
+++ src/main/java/com/example/LinkedList.java
@@ -0,0 +1,29 @@
+package com.example;
+
+public class LinkedList<T> {
+    private Node head ;
+
+    public void append(T data)
+    {
+        if(head == null)
+        {
+            head = new Node(data);
+        }
+        Node current = head;
+        // iterating through the linked list to tail
+        while(current.getNext() != null)
+        {
+            current = current.getNext();
+        }
+        Node newNode = new Node(data);
+        current.setNext(newNode);
+    }
+
+    public void prepend(T data)
+    {
+        Node newNode = new Node(data);
+        newNode.setNext(head);
+        head = newNode;
+    }
+
+}
